1b5ea184f612b0a1244814a2c34897e090071121
b0noI <viacheslav@b0noi.com>
2013-07-15 11:46:27 +0300

    code review comments

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,17 @@
 public class Player {
+    // new lien
     private String name;
+    // add spaces between code lines
     public void setName(String name) {
         if (name != null) {
             this.name = name;
         }
     }
+    // new line
+    // if you overriding methods use @Override annotation
+    @Override
     public String toString() {
         return name;
     }
+    //new line
 }
